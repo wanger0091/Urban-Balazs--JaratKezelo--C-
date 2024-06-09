66432b85f4991f1bc0ab4756245a7ad47939d4e7
wanger0091 <ubalazs30@gmail.com>
2024-06-09 14:59:22 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/JaratKezelo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaratKezeloProject
+{
+    public class JaratKezelo
+    {
+        private Dictionary<string, Jarat> jaratok = new Dictionary<string, Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            if (jaratok.ContainsKey(jaratSzam))
+            {
+                throw new ArgumentException("A járatszám már létezik.");
+            }
+
+            var jarat = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            jaratok[jaratSzam] = jarat;
+        }
+
+        public void Keses(string jaratSzam, int keses)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+            {
+                throw new ArgumentException("A járatszám nem létezik.");
+            }
+
+            var jarat = jaratok[jaratSzam];
+            jarat.Keses += keses;
+
+            if (jarat.Keses < 0)
+            {
+                jarat.Keses -= keses;
+                throw new ArgumentException("A késés nem lehet negatív.");
+            }
+        }
+
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+            {
+                throw new ArgumentException("A járatszám nem létezik.");
+            }
+
+            var jarat = jaratok[jaratSzam];
+            return jarat.Indulas.AddMinutes(jarat.Keses);
+        }
+
+        public List<string> JaratokRepuloterrol(string repter)
+        {
+            var result = new List<string>();
+
+            foreach (var jarat in jaratok.Values)
+            {
+                if (jarat.RepterHonnan == repter)
+                {
+                    result.Add(jarat.JaratSzam);
+                }
+            }
+
+            return result;
+        }
+
+        private class Jarat
+        {
+            public string JaratSzam { get; }
+            public string RepterHonnan { get; }
+            public string RepterHova { get; }
+            public DateTime Indulas { get; }
+            public int Keses { get; set; }
+
+            public Jarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+            {
+                JaratSzam = jaratSzam;
+                RepterHonnan = repterHonnan;
+                RepterHova = repterHova;
+                Indulas = indulas;
+                Keses = 0;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TestJaratKezeloProject/JaratKezeloTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using JaratKezeloProject;
+
+namespace TestJaratKezeloProject
+{
+    [TestFixture]
+    public class JaratKezeloTests
+    {
+        [Test]
+        public void TestUjJarat()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+
+            var result = jaratKezelo.MikorIndul("J123");
+            Assert.That(result, Is.EqualTo(new DateTime(2023, 6, 7, 12, 0, 0)));
+
+            Assert.Throws<ArgumentException>(() => jaratKezelo.UjJarat("J123", "BUD", "LAX", new DateTime(2023, 6, 8, 12, 0, 0)));
+            Assert.Throws<ArgumentException>(() => jaratKezelo.MikorIndul("J999"));
+        }
+
+        [Test]
+        public void TestKeses()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+            jaratKezelo.Keses("J123", 30);
+
+            var result = jaratKezelo.MikorIndul("J123");
+            Assert.That(result, Is.EqualTo(new DateTime(2023, 6, 7, 12, 30, 0)));
+
+            Assert.Throws<ArgumentException>(() => jaratKezelo.Keses("J123", -60));
+            Assert.Throws<ArgumentException>(() => jaratKezelo.Keses("J999", 10));
+        }
+
+        [Test]
+        public void TestMikorIndul()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+            jaratKezelo.Keses("J123", 10);
+            jaratKezelo.Keses("J123", 20);
+
+            var result = jaratKezelo.MikorIndul("J123");
+            Assert.That(result, Is.EqualTo(new DateTime(2023, 6, 7, 12, 30, 0)));
+
+            Assert.Throws<ArgumentException>(() => jaratKezelo.MikorIndul("J999"));
+        }
+
+        [Test]
+        public void TestJaratokRepuloterrol()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("J123", "BUD", "NYC", new DateTime(2023, 6, 7, 12, 0, 0));
+            jaratKezelo.UjJarat("J124", "BUD", "LAX", new DateTime(2023, 6, 8, 12, 0, 0));
+            jaratKezelo.UjJarat("J125", "LAX", "NYC", new DateTime(2023, 6, 9, 12, 0, 0));
+
+            var result = jaratKezelo.JaratokRepuloterrol("BUD");
+
+            Assert.That(result, Contains.Item("J123"));
+            Assert.That(result, Contains.Item("J124"));
+            Assert.That(result, Does.Not.Contain("J125"));
+
+            Assert.That(jaratKezelo.JaratokRepuloterrol("Nem létező reptér."), Is.Empty);
+        }
+    }
+}
